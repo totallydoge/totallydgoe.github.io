ee4e005f66d4f8e1e52fb580f6907fd5a347ddb4
totallydoge <92761105+totallydoge@users.noreply.github.com>
2021-10-25 12:06:35 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 var fieldValue;
 
 function saveName() {
-  //get value of input field and save as "fieldValue"
   fieldValue = document.getElementById('textfield').value
-  //create LS key with value of "fieldValue"
   localStorage.setItem('uName', fieldValue);
-  //automatically take user to page two.html
-  location.href = "two.html";
-}
-
-function getUser() {
-  var b = localStorage.getItem('uName');
-  document.getElementById('placeholder').innerHTML = "Hello " + b;
+  location.href = "page2.html";
 }
